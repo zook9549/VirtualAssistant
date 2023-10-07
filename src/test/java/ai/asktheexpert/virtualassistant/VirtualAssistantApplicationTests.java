004ba0bc9535b68330dc247d3261979e4de98964
Josh Zook <github@joshzook.com>
2023-10-07 14:01:11 -0400

    Removed bad unit tests

--- src/test/java/ai/asktheexpert/virtualassistant/VirtualAssistantApplicationTests.java
+++ src/test/java/ai/asktheexpert/virtualassistant/VirtualAssistantApplicationTests.java
@@ -7,42 +7,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VirtualAssistantApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
-    @Autowired
-    private FileStore fileStore;
-
-    @Test
-    public void testSaveFile() throws Exception {
-        // given
-        String name = "test.mp4";
-        byte[] content = "adfad3".getBytes();
-
-        // when
-        String path = fileStore.save(name, content);
-        System.out.println(path);
-
-    }
-
-    @Test
-    public void testDeleteile() throws Exception {
-        // given
-        String name = "test.mp4";
-        // when
-        boolean result = fileStore.delete(name);
-
-    }
-
-    @Test
-    public void testGetFile() throws Exception {
-        // given
-        String name = "test.mp4";
-        // when
-        byte[] result = fileStore.get(name);
-        System.out.println(new String(result));
-
-    }
-
 }
