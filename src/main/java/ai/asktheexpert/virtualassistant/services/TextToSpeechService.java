683e98f64ba744decacae2a7068e110cc2b37c43
Josh Zook <github@joshzook.com>
2023-11-12 13:06:20 -0500

    Removed unused class

--- src/main/java/ai/asktheexpert/virtualassistant/services/TextToSpeechService.java
+++ src/main/java/ai/asktheexpert/virtualassistant/services/TextToSpeechService.java
@@ -1,7 +1,6 @@
 package ai.asktheexpert.virtualassistant.services;
 
 import ai.asktheexpert.virtualassistant.models.Assistant;
-import ai.asktheexpert.virtualassistant.models.Persona;
 
 public interface TextToSpeechService extends CreditService {
     byte[] getTextToSpeech(Assistant assistant, String text) throws Exception;
